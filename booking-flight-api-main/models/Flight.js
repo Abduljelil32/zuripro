e3280baf308f64215f1d3cb554048cdcba0237b2
Abduljelil32 <85502322+Abduljelil32@users.noreply.github.com>
2022-06-27 18:57:53 +0100

    Add files via upload

---
+++ booking-flight-api-main/models/Flight.js
@@ -0,0 +1,15 @@
+exports.exampleModel = [
+    
+    {
+        title: "flight to canada",
+        time: "1pm",
+        price: 26000,
+        date: "26-06-2022"
+        },{
+            title: "flight to paris",
+            time: "1pm",
+            price: 26000,
+            date: "26-06-2022"
+    }
+        
+];
